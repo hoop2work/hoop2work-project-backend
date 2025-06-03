4a012754d92204acb7955e828110ebe5c6d1207f
timo schneider <timo.schneider@lernende.bms-w.ch>
2025-06-03 14:45:27 +0200

    feat: wip refactor, initial projekt structure

---
+++ src/main/java/com/hoop2work/backend/Hoop2workProjectBackendApplication.java
@@ -0,0 +1,11 @@
+package com.hoop2work.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoop2workProjectBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Hoop2workProjectBackendApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hoop2work/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.hoop2work.backend.repository;
+
+import com.hoop2work.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
